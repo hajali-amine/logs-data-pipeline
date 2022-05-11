49b0a15e0671d2318a7a4e690c6ebc9315fb21b9
hajali-amine <amine.hajali@insat.ucar.tn>
2022-05-11 19:07:26 +0100

    update

--- kafka_appender.py
+++ kafka_appender.py
@@ -2,7 +2,7 @@ from kafka import KafkaProducer, KafkaConsumer
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
-logs = open("logs.txt", "r")
+logs = open("logfiles.log", "r")
 for line in logs:
     producer.send('log', str.encode(line))
 logs.close()
