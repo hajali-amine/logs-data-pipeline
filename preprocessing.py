be0e5096dd020978b3bb63040799b87c7d5f9212
hajali-amine <amine.hajali@insat.ucar.tn>
2022-05-11 19:20:21 +0100

    batch

--- preprocessing.py
+++ preprocessing.py
@@ -1,29 +1,13 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-import random
+from kafka import KafkaProducer, KafkaConsumer
 
+consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
+consumer.subscribe(['log'])
+for msg in consumer:
+    print (msg.value)
 
-spark = SparkSession \
-          .builder \
-          .appName("Preprocessor") \
-          .getOrCreate()
-
-# Subscribe to 1 topic
-# df = spark \
-#   .read \
-#   .format("kafka") \
-#   .option("kafka.bootstrap.servers", "172.27.1.16:9092") \
-#   .option("subscribe", "log") \
-#   .load()
-# df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-import os
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0 pyspark-shell'
-
-df = spark.readStream \
-  .format("kafka") \
-  .option("kafka.bootstrap.servers", "localhost:9092") \
-  .option("subscribe", "log") \
-  .option("startingOffsets", "earliest") \
-  .load()
-
-df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+from pyspark import SparkContext
+sc = SparkContext()
+ma_liste = range(10000)
+rdd = sc.parallelize(ma_liste, 2)
+nombres_impairs = rdd.filter(lambda x: x % 2 != 0)
+nombres_impairs.take(5)
