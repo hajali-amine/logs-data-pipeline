30fc716400baffe0c63c64592482760f8898734e
chaimakr <chaima050@gmail.com>
2022-05-13 00:37:11 +0100

    Revert "preprocessing on hold"
    This reverts commit bdfa3c69699af675ffd697bbaf8c5a6b43e2ea08.

--- .history/kafka_appender_20220511191206.py
+++
@@ -1,9 +0,0 @@
-from kafka import KafkaProducer, KafkaConsumer
-
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-
-logs = open("logfiles.log", "r")
-for line in logs:
-    producer.send('log', str.encode(line))
-logs.close()
-
--- kafka_appender.py
+++ kafka_appender.py
@@ -1,15 +1,9 @@
 from kafka import KafkaProducer, KafkaConsumer
-from json import dumps
-
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
 logs = open("logfiles.log", "r")
-
 for line in logs:
-    #print(line)
-    rep =producer.send('log', str.encode(line))
-    print (rep)
+    producer.send('log', str.encode(line))
 logs.close()
-producer.flush()
 
