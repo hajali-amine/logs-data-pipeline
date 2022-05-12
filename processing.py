c4d68872c4c41fd7805231a92b414111ce48d6f3
hajali-amine <amine.hajali@insat.ucar.tn>
2022-05-12 12:30:50 +0100

    better spark image

--- processing.py
+++ processing.py
@@ -1,4 +1,5 @@
+from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
-sparkSession = SparkSession.builder.master("spark://localhost:7077").appName("example-pyspark-read-and-write").getOrCreate()
-df_load = sparkSession.read.format("csv").option("header", "true").load('hdfs://localhost:50070/input')
+sparkSession = SparkSession.builder.appName("example-pyspark-read-and-write").getOrCreate()
+df_load = sparkSession.read.format("csv").option("header", "true").load('hdfs://namenode:50070/input/out.csv')
 df_load.show()
